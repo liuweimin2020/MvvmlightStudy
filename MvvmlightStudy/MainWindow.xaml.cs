54c9b27efc70ea17baeaed2302ac25eb11afdc5a
伟民 刘 <11680134@qq.com>
2020-02-03 14:03:14 +0800

    添加项目文件。

---
+++ MvvmlightStudy/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace MvvmlightStudy
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Snackbar Snackbar;
+        //public static PaletteSelector2 palette;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(1500);
+            }).ContinueWith(t =>
+            {
+                //note you can use the message queue from any thread, but just for the demo here we 
+                //need to get the message queue from the snackbar, so need to be on the dispatcher
+                MainSnackbar.MessageQueue.Enqueue("Welcome to Material Design In XAML Tookit");
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+
+            //DataContext = new MainWindowViewModel(MainSnackbar.MessageQueue);
+
+            Snackbar = this.MainSnackbar;
+
+            //DataContext = new MainWindowViewModel();
+            //palette = new PaletteSelector2 { DataContext = new PaletteSelectorViewModel() };
+            //tbTheme.DataContext = new PaletteSelectorViewModel();
+
+        }
+
+        private void UIElement_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //until we had a StaysOpen glag to Drawer, this will help with scroll bars
+            var dependencyObject = Mouse.Captured as DependencyObject;
+            while (dependencyObject != null)
+            {
+                if (dependencyObject is ScrollBar) return;
+                dependencyObject = VisualTreeHelper.GetParent(dependencyObject);
+            }
+
+            MenuToggleButton.IsChecked = false;
+        }
+
+        private void OnCopy(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (e.Parameter is string stringValue)
+            {
+                try
+                {
+                    Clipboard.SetDataObject(stringValue);
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+    }
+}
